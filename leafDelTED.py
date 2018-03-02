dbeff738ca91d6bf4aaa8392e287d6fe4ecaa5c5
Mohammad Khaledur Rahman <morahma@ribbon.soic.indiana.edu>
2018-03-02 14:50:40 -0500

    Updated

--- leafDelTED.py
+++ leafDelTED.py
@@ -14,16 +14,17 @@ def createLabels(line):
 	labels = line.strip().replace(" ","").split("=")[1].split(",")
 	return node, labels
 
-def SED(S, T):
+def SAP(S, T):
 	Tab = [[0 for i in range(len(S))] for j in range(len(T))]
 	for i in range(len(S)):
-		Tab[0][i] = 0
 		Tab[i][0] = 0
-	for i in range(len(S)-1):
-		for j in range(len(T)-1):
-			Tab[i+1][j] = max(Tab[i+1][j], Tab[i][j]+len(list(set(S[i]).intersection(set(T[j])))))
-			Tab[i][j+1] = max(Tab[i][j+1], Tab[i][j]+len(list(set(S[i]).intersection(set(T[j])))))
-	print Tab
+	for j in range(len(T)):
+		Tab[0][j] = 0
+	for i in range(1, len(S)):
+		for j in range(1, len(T)):
+			Tab[i][j] = max(Tab[i-1][j], Tab[i][j-1])+len(list(set(S[i]).intersection(set(T[j]))))
+	for r in Tab:
+		print r
 def readTreeFile(pathToFile):
 	assert os.path.exists(pathToFile), "There does not exist file " + pathToFile + ". Function readlabelsAncestryMatrix"
 	treeFile = open(pathToFile)
@@ -46,7 +47,7 @@ if __name__ == '__main__':
 	if len(sys.argv) == 3:
 		l1, n1, T2 = readTreeFile(sys.argv[1])
 		l2, n2, T2 = readTreeFile(sys.argv[2])
-		SED(l1, l2)
+		SAP(l1, l2)
 
 	
 
